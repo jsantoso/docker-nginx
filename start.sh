6298c9b7c8f930aa746eb43795ce033d46fe695e
Jeffrey Santoso <jeffey.k.santoso@gmail.com>
2022-07-21 10:39:34 +1000

    Added support up to 3 php-fpm workers for load-balancing

--- start.sh
+++ start.sh
@@ -2,8 +2,18 @@
 
   export WEB_DIR=${WEB_DIR:-public}
   export FPM_HOST=${FPM_HOST:-php-fpm}
+  export FPM_HOST_2=${FPM_HOST_2:-php-fpm}
+  export FPM_HOST_3=${FPM_HOST_3:-php-fpm}
 
-  # FPM_HOST env injectin - allowing us to set host dynamically on start
-  envsubst '${WEB_DIR},${FPM_HOST}' < /etc/default-fpm.conf > /etc/nginx/conf.d/default.conf
+  if [ $FPM_HOST_2 == 'php-fpm' ]; then
+      export FPM_HOST_2=${FPM_HOST}
+  fi
+
+ if [ $FPM_HOST_3 == 'php-fpm' ]; then
+      export FPM_HOST_3=${FPM_HOST}
+  fi
+
+  # FPM_HOST env injection - allowing us to set host dynamically on start
+  envsubst '${WEB_DIR},${FPM_HOST},${FPM_HOST_2},${FPM_HOST_3}' < /etc/default-fpm.conf > /etc/nginx/conf.d/default.conf
 
   nginx -g 'daemon off;'
