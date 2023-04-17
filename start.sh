3fd04f09fb98aab3984e1c8e4e465297b239b25a
Jeffrey Santoso <jeffey.k.santoso@gmail.com>
2023-04-17 11:40:12 +1000

    Support up to 10 FPM workers

--- start.sh
+++ start.sh
@@ -4,13 +4,43 @@
   export FPM_HOST=${FPM_HOST:-php-fpm}
   export FPM_HOST_2=${FPM_HOST_2:-php-fpm}
   export FPM_HOST_3=${FPM_HOST_3:-php-fpm}
+  export FPM_HOST_4=${FPM_HOST_4:-php-fpm}
+  export FPM_HOST_5=${FPM_HOST_5:-php-fpm}
+  export FPM_HOST_6=${FPM_HOST_6:-php-fpm}
+  export FPM_HOST_7=${FPM_HOST_7:-php-fpm}
+  export FPM_HOST_8=${FPM_HOST_8:-php-fpm}
+  export FPM_HOST_9=${FPM_HOST_9:-php-fpm}
 
   if [ $FPM_HOST_2 == 'php-fpm' ]; then
       export FPM_HOST_2=${FPM_HOST}
   fi
 
- if [ $FPM_HOST_3 == 'php-fpm' ]; then
+  if [ $FPM_HOST_3 == 'php-fpm' ]; then
       export FPM_HOST_3=${FPM_HOST}
+  fi
+
+  if [ $FPM_HOST_4 == 'php-fpm' ]; then
+      export FPM_HOST_4=${FPM_HOST}
+  fi
+
+  if [ $FPM_HOST_5 == 'php-fpm' ]; then
+      export FPM_HOST_5=${FPM_HOST}
+  fi
+
+  if [ $FPM_HOST_6 == 'php-fpm' ]; then
+      export FPM_HOST_6=${FPM_HOST}
+  fi
+
+  if [ $FPM_HOST_7 == 'php-fpm' ]; then
+      export FPM_HOST_7=${FPM_HOST}
+  fi
+
+  if [ $FPM_HOST_8 == 'php-fpm' ]; then
+      export FPM_HOST_8=${FPM_HOST}
+  fi
+
+  if [ $FPM_HOST_9 == 'php-fpm' ]; then
+      export FPM_HOST_9=${FPM_HOST}
   fi
 
   # FPM_HOST env injection - allowing us to set host dynamically on start
