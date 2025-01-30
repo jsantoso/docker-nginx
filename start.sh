704a501684ba81e316093be15722f279cb2c09a4
Jeffrey Santoso <jeffey.k.santoso@gmail.com>
2025-01-30 18:44:45 +1100

    Make upstream declaration dynamic based on environment variables available

--- start.sh
+++ start.sh
@@ -1,49 +1,36 @@
 #!/bin/bash
 
   export WEB_DIR=${WEB_DIR:-public}
-  export FPM_HOST=${FPM_HOST:-php-fpm}
-  export FPM_HOST_2=${FPM_HOST_2:-php-fpm}
-  export FPM_HOST_3=${FPM_HOST_3:-php-fpm}
-  export FPM_HOST_4=${FPM_HOST_4:-php-fpm}
-  export FPM_HOST_5=${FPM_HOST_5:-php-fpm}
-  export FPM_HOST_6=${FPM_HOST_6:-php-fpm}
-  export FPM_HOST_7=${FPM_HOST_7:-php-fpm}
-  export FPM_HOST_8=${FPM_HOST_8:-php-fpm}
-  export FPM_HOST_9=${FPM_HOST_9:-php-fpm}
+  export FPM_SERVERS=''
 
-  if [ $FPM_HOST_2 == 'php-fpm' ]; then
-      export FPM_HOST_2=${FPM_HOST}
+  FPM_PORT=9000
+  # Leading white space. \t doesn't work right.
+  LWS='    '
+  # Line return. \n doesn't work right.
+  LR='
+  '
+
+  # Gather all environment variables beginning with FPM_HOST
+  FPM_HOSTS_VARS=`env | grep '^FPM_HOST'`
+
+  if [[ $FPM_HOSTS_VARS == '' ]]
+  then
+      # Use 'php-fpm' as the server name if there are no matching env vars.
+      FPM_SERVERS="${LWS}server php-fpm:${FPM_PORT}"
+  else
+      # If there are environment variables starting with FPM_HOST, then create a
+      # string that contains all of them, in the right format for the nginx conf.
+      for FPM_HOST_VAR in $FPM_HOSTS_VARS
+      do
+        VALUE=`echo $FPM_HOST_VAR | cut -d '=' -f 2`
+        FPM_SERVERS+="${LWS}server ${VALUE}:${FPM_PORT};${LR}"
+      done
+      # Trim the trailing newline
+      FPM_SERVERS=${FPM_SERVERS%$'\n'}
   fi
 
-  if [ $FPM_HOST_3 == 'php-fpm' ]; then
-      export FPM_HOST_3=${FPM_HOST}
-  fi
-
-  if [ $FPM_HOST_4 == 'php-fpm' ]; then
-      export FPM_HOST_4=${FPM_HOST}
-  fi
-
-  if [ $FPM_HOST_5 == 'php-fpm' ]; then
-      export FPM_HOST_5=${FPM_HOST}
-  fi
-
-  if [ $FPM_HOST_6 == 'php-fpm' ]; then
-      export FPM_HOST_6=${FPM_HOST}
-  fi
-
-  if [ $FPM_HOST_7 == 'php-fpm' ]; then
-      export FPM_HOST_7=${FPM_HOST}
-  fi
-
-  if [ $FPM_HOST_8 == 'php-fpm' ]; then
-      export FPM_HOST_8=${FPM_HOST}
-  fi
-
-  if [ $FPM_HOST_9 == 'php-fpm' ]; then
-      export FPM_HOST_9=${FPM_HOST}
-  fi
 
   # FPM_HOST env injection - allowing us to set host dynamically on start
-  envsubst '${WEB_DIR},${FPM_HOST},${FPM_HOST_2},${FPM_HOST_3},${FPM_HOST_4},${FPM_HOST_5},${FPM_HOST_6},${FPM_HOST_7},${FPM_HOST_8},${FPM_HOST_9}' < /etc/default-fpm.conf > /etc/nginx/conf.d/default.conf
+  envsubst '${WEB_DIR},${FPM_SERVERS}' < /etc/default-fpm.conf > /etc/nginx/conf.d/default.conf
 
   nginx -g 'daemon off;'
